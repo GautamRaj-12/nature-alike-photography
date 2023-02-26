fa24c7d3258e4d953a808e2580ad48f67875adcd
GautamRaj-12 <gautamraj996@gmail.com>
2023-02-26 11:19:55 +0530

    responsiveness done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,32 @@
-import React from "react";
-import insectOnPlant from "../../assets/insect-on-plant.jpg";
-import dropsOnPlant from "../../assets/drops-on-plant.jpg"
-import insectOnPlantDark from "../../assets/insect-on-plant-dark.jpg"
-import "./main.css";
+import React from 'react';
+import insectOnPlant from '../../assets/insect-on-plant.jpg';
+import dropsOnPlant from '../../assets/drops-on-plant.jpg';
+import insectOnPlantDark from '../../assets/insect-on-plant-dark.jpg';
+import './main.css';
 function Main() {
   return (
     <>
-      <main className="main">
-        <div class="main-text">
-          Nature
+      <main className='main'>
+        <div class='main-text'>
+          Nature Alike Photography - Capturing the beauty of Nature through
+          Lens.
         </div>
-        <div class="main-images">
-          <img src={insectOnPlant} alt="An insect on a plant" class="first-img"/>
-          <img src={dropsOnPlant} alt="Raindrops on a plant" class="second-img"/>
-          <img src={insectOnPlantDark} alt="An insect on a plant in the evening" class="third-img"/>
+        <div class='main-images'>
+          <img
+            src={insectOnPlant}
+            alt='An insect on a plant'
+            class='first-img'
+          />
+          <img
+            src={dropsOnPlant}
+            alt='Raindrops on a plant'
+            class='second-img'
+          />
+          <img
+            src={insectOnPlantDark}
+            alt='An insect on a plant in the evening'
+            class='third-img'
+          />
         </div>
       </main>
     </>
